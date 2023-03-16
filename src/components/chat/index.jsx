062f9e8e1a984271d65406329dfa3c36f4b70951
itx-shoaib <shoaibjamil43@gmail.com>
2023-03-17 02:26:32 +0500

    Making index.jsx of chat

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
 
 const Chat = () => {
+    const chatProps = useMultiChatLogic(
+        import.meta.env.VITE_PROJECT_ID,
+        "testuser",
+        "123"
+    )
     return (
-        <div>Chat</div>
+        <div style={{ flexBasis: "100%" }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow
+                {...chatProps}
+                style={{ height: "100vh" }}
+                renderChatHeader={(chat) => <Header chat={chat} />}
+
+            />
+        </div>
     )
 }
 
