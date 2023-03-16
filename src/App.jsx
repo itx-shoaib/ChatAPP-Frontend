51e4d93b2af3ddf5ab0777426e105c3c6f57e9a0
itx-shoaib <shoaibjamil43@gmail.com>
2023-03-17 02:14:49 +0500

    Making jsconfig.json file and adding @ path information

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
-import Chat from "./components/chat"
+import Chat from "@/components/chat"
 
 function App() {
   return (
