72d0c5957614e05569e6c7546d8a872cc3623da3
itx-shoaib <shoaibjamil43@gmail.com>
2023-03-20 00:20:17 +0500

    Updating the standardMessageForm.jsx

--- src/components/customMessageForms/standardMessageForm.jsx
+++ src/components/customMessageForms/standardMessageForm.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { XMarkIcon } from "@heroicons/react/24/solid"
 
 const StandardMessageForm = () => {
+  const [message, setmessage] = useState("");
+  const [attachment, setattachment] = useState("");
+  const [preview, setpreview] = useState("");
   return (
-    <div>standardMessageForm</div>
+    <div className="message-form-container">
+      {preview && (
+        <div className="message-form-preview">
+          <img src={preview} alt="No" className="message-form-preview-image" onLoad={() => URL.revokeObjectURL(preview)} />
+          <XMarkIcon className="message-form-icon-x" onClick={() => {
+            setpreview("");
+            setattachment("");
+          }} />
+        </div>
+      )}
+    </div>
   )
 }
 
